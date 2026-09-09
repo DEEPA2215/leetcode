177c2aa4881f1285014d3ac35ac52def4586c997
DEEPA2215 <deepasr15072006@gmail.com>
2026-09-09 14:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.69%) - LeetSync

---
+++ 476-number-complement/number-complement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        binary=bin(num)[2:]
+        complement=""
+        for i in binary:
+            if i=='0':
+                complement+='1'
+            if i=='1':
+                complement+='0'
+        return int(complement,2)           
+        
